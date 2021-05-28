f44f8ff4879729e21a3729536e4e7386c1506037
Katrin Fritz <9885028+katrinundfritz@users.noreply.github.com>
2021-05-28 15:47:54 +0200

    Style header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,15 +12,17 @@ const Header = () => {
     <header>
       <SuperHeader />
       <MainHeader>
-        <Logo />
-        <Nav>
-          <NavLink href="/sale">Sale</NavLink>
-          <NavLink href="/new">New&nbsp;Releases</NavLink>
-          <NavLink href="/men">Men</NavLink>
-          <NavLink href="/women">Women</NavLink>
-          <NavLink href="/kids">Kids</NavLink>
-          <NavLink href="/collections">Collections</NavLink>
-        </Nav>
+        <MainHeaderContent>
+          <Logo />
+          <Nav>
+            <NavLink href="/sale">Sale</NavLink>
+            <NavLink href="/new">New&nbsp;Releases</NavLink>
+            <NavLink href="/men">Men</NavLink>
+            <NavLink href="/women">Women</NavLink>
+            <NavLink href="/kids">Kids</NavLink>
+            <NavLink href="/collections">Collections</NavLink>
+          </Nav>
+        </MainHeaderContent>
       </MainHeader>
     </header>
   );
@@ -29,9 +31,22 @@ const Header = () => {
 const MainHeader = styled.div`
   padding: 0 32px;
   border-bottom: 1px solid ${COLORS.gray[300]};
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 72px;
 `;
 
-const Nav = styled.nav``;
+const MainHeaderContent = styled.div`
+  display: flex;
+  align-items: baseline;
+`
+
+const Nav = styled.nav`
+  display: flex;
+  gap: 16px;
+  margin-left: 200px;
+`;
 
 const NavLink = styled.a`
   font-size: 1.125rem;
